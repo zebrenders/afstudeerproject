26ac0d24f41f5a4b35c05192ad22b74ae3c2fb82
zebrenders <zeb.renders@gmail.com>
2025-03-11 12:01:06 +0100

    new

--- afstudeer_project/src/main.cpp
+++ afstudeer_project/src/main.cpp
@@ -1,18 +1,60 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <PubSubClient.h>
+#include <secrets.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define dht20
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// MQTT message handler
+void handleMessage(char *topic, byte *payload, unsigned int length)
+{
+  String message = "";
+  for (unsigned int i = 0; i < length; i++)
+  {
+    message += (char)payload[i];
+  }
+  Serial.println("Received message on topic [" + String(topic) + "]: " + message);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// MQTT reconnect function
+void reconnect()
+{
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP32Client"))
+    {
+      Serial.println("connected");
+      client.subscribe("testTopic");
+    }
+    else
+    {
+      Serial.println("failed, rc=" + String(client.state()) + " retrying in 5 seconds");
+      delay(5000);
+    }
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void setup_wifi()
+{
+    WiFi.begin(ssid,password);
+    Serial.println("Connected to WiFi!");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  // Setup MQTT
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(handleMessage);
+}
+
+void loop()
+{
 }
---
+++ afstudeer_project/src/secrets.h
@@ -0,0 +1,9 @@
+
+// Replace the next variables with your SSID/Password combination
+const char *ssid = "IoT";
+const char *password = "KdGIoT8!";
+
+// Add your MQTT Broker IP address, example:
+// const char* mqtt_server = "192.168.1.144";
+const char *mqtt_server = "10.6.120.23";
+#define mqtt_port  1884
