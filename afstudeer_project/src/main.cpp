4e644fdfae9a913cd3913114b53621b49a3bfda4
zebrenders <zeb.renders@gmail.com>
2025-03-13 16:11:11 +0100

    main

--- afstudeer_project/src/main.cpp
+++ afstudeer_project/src/main.cpp
@@ -2,12 +2,56 @@
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <PubSubClient.h>
+#include "HTTPClient.h"
+#include <DHT.h>
 #include <secrets.h>
 
-#define dht20
+#define DHTPIN D10
+#define DHTTYPE DHT22
+
+// test
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+DHT dht(DHTPIN, DHTTYPE);
+
+
+// Google Apps Script URL
+const String GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/";
+
+// Keep track of number of requests
+int requestCounter = 0;
+int temp = 20;
+int hum = 30;
+int power;
+
+void send_data_https()
+{
+
+  // Create URL with parameters to call Google Apps Script
+  String urlFinal = GOOGLE_APPS_SCRIPT_URL + GOOGLE_SCRIPT_MACRO_ID + "/exec?tijdstip=" + String(requestCounter) + "&temperatuur=" + String(temp) + "&humidity=" + String(hum);
+
+  Serial.print("POST data to spreadsheet: ");
+  Serial.println(urlFinal);
+
+  // Send HTTP request
+  HTTPClient http;
+  http.begin(urlFinal.c_str());
+  // http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
+  int httpCode = http.GET();
+  Serial.print("HTTP Status Code: ");
+  Serial.println(httpCode);
+
+  // Get response from HTTP request
+  String payload;
+  if (httpCode > 0)
+  {
+    payload = http.getString();
+    Serial.println("Payload: " + payload);
+  }
+  http.end();
+}
 
 // MQTT message handler
 void handleMessage(char *topic, byte *payload, unsigned int length)
@@ -41,15 +85,47 @@ void reconnect()
 
 void setup_wifi()
 {
-    WiFi.begin(ssid,password);
-    Serial.println("Connected to WiFi!");
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void send_data_mqtt()
+{
+  power = random(0, 600);
+  char value[8];
+  dtostrf(power, 1, 2, value);
+  client.subscribe("value");
+  // client.subscribe("testTopic");
+  client.publish("power", value);
+}
+
+void get_dht(){
+temp = dht.readTemperature();    
+hum = dht.readHumidity();
+
 }
 
 void setup()
 {
   Serial.begin(115200);
+  setup_wifi();
+
   // Setup MQTT
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(handleMessage);
@@ -57,4 +133,11 @@ void setup()
 
 void loop()
 {
+
+  // Handle MQTT connection
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
 }
--- afstudeer_project/src/secrets.h
+++ afstudeer_project/src/secrets.h
@@ -7,3 +7,10 @@ const char *password = "KdGIoT8!";
 // const char* mqtt_server = "192.168.1.144";
 const char *mqtt_server = "10.6.120.23";
 #define mqtt_port  1884
+
+
+#define GOOGLE_SCRIPT_ID "1A8mhwrBXst_taBZ0lMG23JrmGmECfS2BQ27O3Uxvj50"
+
+#define GOOGLE_SCRIPT_MACRO_ID "AKfycbzAiaMDvLGDELBEbBCH-WlYnSO1PC3LpltTid_bNMH_-Oh2r2ksS0GVBwgXKxEaslqpGw"
+
+//https://script.google.com/macros/s/AKfycbzAiaMDvLGDELBEbBCH-WlYnSO1PC3LpltTid_bNMH_-Oh2r2ksS0GVBwgXKxEaslqpGw/exec?tijdstip=00&temperatuur=20&humidity=30
