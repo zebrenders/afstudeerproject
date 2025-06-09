18619a3d8e6c5549e4a68dd728cbbfbf484de810
zebrenders <zeb.renders@gmail.com>
2025-06-09 14:12:31 +0200

    Update secrets.h

--- src/secrets.h
+++ src/secrets.h
@@ -1,19 +1,9 @@
 
-// Replace the next variables with your SSID/Password combination
+
 const char *ssid = "WIFI_ZEB";
 const char *password = "PASSWORD123123";
 
-// Add your MQTT Broker IP address, example:
 const char *mqtt_server = "zebrenders.local";
 
-
-
-// const char *mqtt_server = "192.168.86.58";
 #define mqtt_port  1881
 
-
-#define GOOGLE_SCRIPT_ID "1A8mhwrBXst_taBZ0lMG23JrmGmECfS2BQ27O3Uxvj50"
-
-#define GOOGLE_SCRIPT_MACRO_ID "AKfycbzAiaMDvLGDELBEbBCH-WlYnSO1PC3LpltTid_bNMH_-Oh2r2ksS0GVBwgXKxEaslqpGw"
-
-//https://script.google.com/macros/s/AKfycbzAiaMDvLGDELBEbBCH-WlYnSO1PC3LpltTid_bNMH_-Oh2r2ksS0GVBwgXKxEaslqpGw/exec?tijdstip=00&temperatuur=20&humidity=30
